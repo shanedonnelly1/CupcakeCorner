268ed5ab6a180754a4410be987682c0b72d4b156
Shane Donnelly <shanedonnelly1@hotmail.com>
2020-10-12 13:56:19 +0100

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Shane on 12/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
